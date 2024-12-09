08074cdb528d9113903398df3150352ed02d8a11
Ali <alikamali0328@gmail.com>
2024-12-09 15:55:16 +0330

    Step 2: Configuring Axios and creating a context and rendering the list of products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,22 @@
+import { useProducts } from "../context/ProductContext";
+
+import styles from "./ProductsPage.module.css"
+
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const products = useProducts();
+  console.log(products);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map((p) =>( 
+          <p key={p.id}>{p.title}</p>
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
