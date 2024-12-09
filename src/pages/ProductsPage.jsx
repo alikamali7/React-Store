831a1488b9ecc58b0c37b1ecb3a2ed68e32b3ec6
Ali <alikamali0328@gmail.com>
2024-12-09 19:33:37 +0330

    Step 4: Data management to create the possibility of searching and creating status queries and multi-step filtering of products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ImSearch } from "react-icons/im";
 import { FaListUl } from "react-icons/fa";
 
@@ -6,24 +6,37 @@ import Card from "../components/Card";
 import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 
+import { searchProducts, filterProducts } from "../helpers/helper"
 import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
 
+  const [displayed, setDisplayed] = useState([]);
   const [search, setSearch] = useState();
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category)
+
+    setDisplayed(finalProducts)
+  }, [query])
 
   const searchHandler = () => {
-    console.log("first");
+    setQuery((query) => ({ ...query, search }));
   };
 
   const categoryHandler = (event) => {
     const { tagName } = event.target;
     const category = event.target.innerText.toLowerCase();
-    console.log(tagName);
-    console.log(category);
-    if (tagName !== "LI") return;
 
+    if (tagName !== "LI") return;
+    setQuery(query => ({...query, category}))
   };
 
   return (
@@ -41,8 +54,8 @@ function ProductsPage() {
       </div>
       <div className={styles.container}>
         <div className={styles.products}>
-          {!products.length && <Loader />}
-          {products.map((p) => (
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
             <Card key={p.id} data={p} />
           ))}
         </div>
