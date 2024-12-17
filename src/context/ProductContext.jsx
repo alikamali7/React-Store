576e84a5c39c4ae30cbf0b1f64908b769509cc08
Ali <alikamali0328@gmail.com>
2024-12-17 15:46:49 +0330

    Step 8: Implement the product details page and the shopping cart page

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import api from "../services/config"
+import api from "../services/config";
 
 const ProductContext = createContext();
 
@@ -31,5 +31,11 @@ const useProducts = () => {
   return products;
 };
 
+const useProductDetails = (id) => {
+  const products = useContext(ProductContext);
+  const result = products.find((product) => product.id === id);
+  return result;
+};
+
 export default ProductsProvider;
-export { useProducts };
+export { useProducts, useProductDetails };
